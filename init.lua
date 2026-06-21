1b1e8b81281dad7efde52e87ec1fb23e84db0dc7
Max Unterein <m.unterein@elcomnet.de>
2026-06-21 15:09:11 +0200

    changed indentation to 2 spaces for defined files

--- init.lua
+++ init.lua
@@ -176,9 +176,9 @@ do
   vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'c', 'cpp' },
     callback = function()
-      vim.bo.tabstop = 4
-      vim.bo.shiftwidth = 4
-      vim.bo.softtabstop = 4
+      vim.bo.tabstop = 2
+      vim.bo.shiftwidth = 2
+      vim.bo.softtabstop = 2
       vim.bo.expandtab = true
     end,
   })
